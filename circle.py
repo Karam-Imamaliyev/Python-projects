1374f952cea74ea6845e3435a3b73f3a94df90c2
Karam0001 <77601687+Karam0001@users.noreply.github.com>
2022-05-01 01:01:54 +0200

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,9 @@
+import math
+
+radius=float(input("Please enter the radius: "))
+
+area=math.pi*(radius**2)
+circumference=2*math.pi*radius
+
+print("The area of circle is" ,round(area,2))
+print("The circumference of tge circle is ", round(circumference,2))
